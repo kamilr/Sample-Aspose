527eb159efbadb47fac0a74ea876604e65a86a33
Kamil Rejczyk <kamil.rejczyk@synaway.com>
2013-09-19 16:04:48 +0200

    work on pptx format opening, modyfing and savin first slide of raport template (text changes)

--- src/com/synaway/simpleaspose/Klasa.java
+++ src/com/synaway/simpleaspose/Klasa.java
@@ -13,7 +13,6 @@ import com.aspose.slides.Shape;
 import com.aspose.slides.ShapeTypeEx;
 import com.aspose.slides.Slide;
 import com.aspose.slides.SlideEx;
-import com.aspose.slides.Slides;
 import com.aspose.slides.SlidesEx;
 import com.aspose.slides.Collections.ICollection;
 
@@ -36,26 +35,24 @@ public class Klasa {
         
         System.out.println("wow, hello.ppt was created");
 
-        Presentation presopen = new Presentation(dataDir + "hello.ppt");
+        PresentationEx presopen = new PresentationEx(dataDir + "Level_3_VP_Tool_Report_Template.pptx");
         
-        //Printing the total number of slides present in the presentation
+
         System.out.println("slides count is "+Integer.toString(presopen.getSlides().size()));
    
         PresentationEx pres1 = new PresentationEx();
         pres1 = presopen;
-        //....do some work here.....
         processPresentation(pres1);
 //
-//        //Save your presentation to a file
+//        
 //        pres1.write(dataDir + "toFile.pptx");
 //
 //        System.out.println("Presentation saved to a file successfully.");
 //        ___________________________________
         try
         {
-            FileOutputStream outStream = new FileOutputStream(dataDir + "toStream2.ppt");
+            FileOutputStream outStream = new FileOutputStream(dataDir + "toStream2.pptx");
 
-            //Saving the presentation to the output stream of Http Response
             pres1.write(outStream);
             outStream.close();
 
@@ -67,31 +64,24 @@ public class Klasa {
         }
 //        ________________________________-
    }
-    public static void processPresentation(Presentation pres)
+    public static void processPresentation(PresentationEx pres)
     {
         int lastSlideNr = pres.getSlides().size();
         
-        Slides slds = pres.getSlides();
-       
-        ((Presentation) slds).addEmptySlide();
-        //Get the first slide
-        SlideEx sld = pres.getSlides().get_Item(lastSlideNr+1);
+        LayoutSlideEx arg0;
 
-        //Add an AutoShape of Rectangle type
+        SlideEx sld = pres.getSlides().get_Item(0);
+
         int idx = sld.getShapes().addAutoShape(ShapeTypeEx.Rectangle, 150, 75,150 , 50);
         AutoShapeEx ashp = (AutoShapeEx)sld.getShapes().get_Item(idx);
 
-        //Add TextFrame to the Rectangle
         ashp.addTextFrame("Hello World");
 
-        //Change the text color to Black (which is White by default)
         ashp.getTextFrame().getParagraphs().get_Item(0).getPortions().get_Item(0).getFillFormat().setFillType(FillTypeEx.Solid);
         ashp.getTextFrame().getParagraphs().get_Item(0).getPortions().get_Item(0).getFillFormat().getSolidFillColor().setColor(java.awt.Color.black);
 
-        //Change the line color of the rectangle to White
         ashp.getShapeStyle().getLineColor().setColor(java.awt.Color.white);
 
-        //Remove any fill formatting in the shape
         ashp.getFillFormat().setFillType(FillTypeEx.NoFill);
     }
 }
